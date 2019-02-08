ebd83275361f083bd953bedd2640f19806bf3b52
Krishnakumar K P <krishnakumarp@cybage.com>
2019-02-08 18:18:05 +0530

    docker files for lamp dev env setup

---
+++ bootstrap.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# create project folder
+sudo mkdir "/home/krishna"
+sudo apt-get update
+sudo apt-get upgrade
+
+sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+sudo apt update
+sudo apt install -y docker-ce
+sudo usermod -aG docker 
+
+sudo curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose${USER}
+sudo chmod +x /usr/local/bin/docker-compose
+
+base=https://github.com/docker/machine/releases/download/v0.16.0 && curl -L $base/docker-machine-$(uname -s)-$(uname -m) >/tmp/docker-machine && sudo install /tmp/docker-machine /usr/local/bin/docker-machine
+
+
